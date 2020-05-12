8faa8ea539059fef73573ca1c91b3ab414815adc
FiNSFlexin <ceohitman0905@gmail.com>
2020-05-11 20:34:54 -0400

    make it better

--- external/help.js
+++ external/help.js
@@ -15,7 +15,7 @@ module.exports = {
                     .setColor (`0x${color}`)
                     .setTitle ('Help Command - Travel')
                     .setDescription ('The **travel command** allows you to travel to one state to another through various modes of transportation. These have their respective advantages and disadvantages with each.')
-                    .addField ('Usage', 'The travel command is only to be used in their respective locations: `-<method> <location> <location> [additional]` or try `-drv tx ca`.')
+                    .addField ('Usage', 'The travel command is only to be used in their respective locations: `-<method> <location> [additional]` or try `-drv ca`.')
                     .addField ('<method>', '**drv**: Car\n**fly**: Plane\n**rde**: Train\n**sal**: Boat', true)
                     .addField ('<location>', '`-help transport places`\n**??**: 2-letter acronym\n**rn**: random location', true)
                     .setTimestamp ()
@@ -30,10 +30,10 @@ module.exports = {
                     .setColor (`0x${color}`)
                     .setTitle ('Help Command - Travel - Location')
                     .setDescription ('The location argument of the **travel command** states the place where you are coming from and where you will be going, it could either be the 50 states or other selected places.')
-                    .addField ('Usage', 'The location argument of the travel command is used with the travel command: `-<method> <location> <location> [additional]` or try `-drv tx ca`.')
-                    .addField ('<location>', '**al**: Alabama\n**ak**: Alaska\n**az**: Arizona\n**ar**: Arkansas\n**ca**: California\n**co**: Colorado\n**ct**: Connecticut\n**de**: Delaware\n**fl**: Florida\n**ga**: Georgia\n**hi**: Hawaii\n**id**: Idaho\n**il**: Illinois\n**in**: Indiana\n**ia**: Iowa\n**ks**: Kansas\n**ky**: Kentucky\n**la**: Louisiana', true)
-                    .addField ('** **', '**me**: Maine\n**md**: Maryland\n**ma**: Massachusetts\n**mi**: Michigan\n**mn**: Minnesota\n**ms**: Mississippi\n**mo**: Missouri\n**mt**: Montana\n**ne**: Nebraska\n**nv**: Nevada\n**nh**: New Hampshire\n**nj**: New Jersey\n**nm**: New Mexico\n**ny**: New York\n**nc**: North Carolina\n**nd**: North Dakota\n**oh**: Ohio\n**ok**: Oklahoma\n', true)
-                    .addField ('** **', '**or**: Oregon\n**pa**: Pennsylvania\n**ri**: Rhode Island\n**sc**: South Carolina\n**sd**: South Dakota\n**tn**: Tennessee\n**tx**: Texas\n**ut**: Utah\n**vt**: Vermont\n**va**: Virginia\n**wa**: Washington\n**wv**: West Virginia\n**wi**: Wisconsin\n**wy**: Wyoming\n**dc**: District of Columbia\n**pr**: Puerto Rico\n**mx**: Mexico\n**cn**: Canada', true)
+                    .addField ('Usage', 'The location argument of the travel command is used with the travel command: `-<method> <location> [additional]` or try `-drv tx ca`.')
+                    .addField ('<location>', '**al**: Alabama\n**ak**: Alaska\n**az**: Arizona\n**ar**: Arkansas\n**ca**: California\n**co**: Colorado\n**ct**: Connecticut\n**de**: Delaware\n**fl**: Florida\n**ga**: Georgia\n**hi**: Hawaii\n**id**: Idaho\n**il**: Illinois\n**in**: Indiana\n**ia**: Iowa\n**ks**: Kansas\n**ky**: Kentucky', true)
+                    .addField ('** **', '**la**: Louisiana\n**me**: Maine\n**md**: Maryland\n**ma**: Massachusetts\n**mi**: Michigan\n**mn**: Minnesota\n**ms**: Mississippi\n**mo**: Missouri\n**mt**: Montana\n**ne**: Nebraska\n**nv**: Nevada\n**nh**: New Hampshire\n**nj**: New Jersey\n**nm**: New Mexico\n**ny**: New York\n**nc**: North Carolina\n**nd**: North Dakota', true)
+                    .addField ('** **', '**oh**: Ohio\n**ok**: Oklahoma\n**or**: Oregon\n**pa**: Pennsylvania\n**ri**: Rhode Island\n**sc**: South Carolina\n**sd**: South Dakota\n**tn**: Tennessee\n**tx**: Texas\n**ut**: Utah\n**vt**: Vermont\n**va**: Virginia\n**wa**: Washington\n**wv**: West Virginia\n**wi**: Wisconsin\n**wy**: Wyoming\n**rn**: Random', true)
                     .setTimestamp ()
                     message.channel.send ({embed}).then (sentMessage => { // Sends the embed message
                         sentMessage.delete({timeout: config.autodelete.help});
@@ -46,7 +46,7 @@ module.exports = {
                     .setColor (`0x${color}`)
                     .setTitle ('Help Command - Travel - Method')
                     .setDescription ('The method argument of the **travel command** states how you will be traveling, some methods may not be able to travel to certain locations.')
-                    .addField ('Usage', 'The method argument of the travel command is used with the travel command: `-<method> <location> <location> [additional]` or try `-drv tx ca`.')
+                    .addField ('Usage', 'The method argument of the travel command is used with the travel command: `-<method> <location> [additional]` or try `-drv tx ca`.')
                     .addField ('<method>', '**drv**: Drive in a car to a nearby state\n**fly**: Fly in a plane to a distant state\n**rde**: Ride in a HSR train to a select state\n**sal**: Sail in a boat to a coastal state')
                     .setTimestamp ()
                     message.channel.send ({embed}).then (sentMessage => { // Sends the embed message
--- index.js
+++ index.js
@@ -53,42 +53,68 @@ client.on ('guildMemberAdd', member => {
     })
 })
 
-// Activates when a message is sent
+// Commands
 client.on ('message', message => {
     if (!message.channel.type == 'text' || message.author.bot || !message.content.startsWith(config.prefix)) return; // Checks if message was sent in server, by human, with prefix
     let lowermessage = message.content.toLowerCase (); // Lowercases the message
     let args = lowermessage.slice(config.prefix.length).split(/ +/); // Seperates the message into arguments
     const externalex = client.external.find (ext => ext.summoner && ext.summoner.includes (args[0])) // Gets 'summoner' from other files (ways it can be identified)
-    if (!cooldowns.has(externalex.name)) {
-		cooldowns.set(externalex.name, new discord.Collection());
+    if (externalex == null) return message.reply ('There was an error with the command').then (sentMessage => {  // Error with defining command
+        sentMessage.delete({timeout: config.autodelete.sent});
+        message.delete ({timeout: config.autodelete.received});
+    });
+    if (!cooldowns.has(externalex.name)) { // Gets name of command
+		cooldowns.set(externalex.name, new discord.Collection()); // A collection of the cooldown
     }
-    const now = Date.now();
-	const timestamps = cooldowns.get (externalex.name);
-    const cooldownAmount = (externalex.cooldown || 3) * 1000;
-    if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-		if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            let reply = '';
-            if (timeLeft.toFixed(1) == 1) reply = `Wait ${timeLeft.toFixed(1)} more second before reusing the \`-${externalex.name}\` command.`;
-            else reply = `Wait ${timeLeft.toFixed(1)} more seconds before reusing the \`-${externalex.name}\` command.`;
+    const now = Date.now(); // Get the time now
+	const timestamps = cooldowns.get (externalex.name); // Get the cooldowns for a specific thing
+    const cooldownAmount = (externalex.cooldown || 3) * 1000; // Length of cooldown
+    if (timestamps.has(message.author.id)) { // Checks personwise
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount; // time till it cooldown ends
+		if (now < expirationTime) { // Checks if cooldown checks good!
+            const timeLeft = (expirationTime - now) / 1000; // Time left
+            let reply = ''; // Defines reply as nothing
+            if (timeLeft.toFixed(1) == 1) reply = `Wait ${timeLeft.toFixed(1)} more second before reusing the \`-${externalex.name}\` command.`; // If one second is left exactly
+            else reply = `Wait ${timeLeft.toFixed(1)} more seconds before reusing the \`-${externalex.name}\` command.`; // If not one second is left
             return message.reply (reply).then (sentMessage => {  // Error with command
                 sentMessage.delete({timeout: timeLeft + 3500});
                 message.delete ({timeout: timeLeft + 4000});
             })
         }
 	}
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	timestamps.set(message.author.id, now); // Record the time
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount); // Deletes the timestamp when it should be over
     try {
         externalex.execute (message, args) // Execute to external file
     }
     catch {
-        return message.channel.send (`${message.author} There was an error with the command`).then (sentMessage => {  // Error with command
+        return message.reply ('There was an error with the command').then (sentMessage => {  // Error with command
             sentMessage.delete({timeout: config.autodelete.sent});
             message.delete ({timeout: config.autodelete.received});
         });
     }
+})
+
+// Channel based
+client.on ('message', message => {
+    if (message.channel.parent.id == config.categorys.states) {
+        const stateswebhook = new discord.WebhookClient(config.webhooks.statesid, config.webhooks.statestoken);
+        let channelm = message.channel.name
+        channelm = channelm.replace(/-/g, ' ');
+        channelm = channelm.replace(/(^\w|\s\w)/g, m => m.toUpperCase());
+        let colors = ['d4002c', '004dc9', 'fefefe']; // American colors
+        let color = colors [Math.floor(Math.random() * colors.length)]; // Random color
+        let onehourmessages = message.createdTimestamp - 3600000;
+        let messages100 = message.channel.messages.fetch ({limit: 100}).then ((messages) => (messages = messages.filter(msg => msg.created >= onehourmessages)));
+        const embed = new discord.MessageEmbed() // Creating embed that mimics people
+        .setAuthor (message.author.username, message.author.displayAvatarURL({format: "png", dynamic: true}))
+        .setColor (`0x${color}`)
+        .setTitle (`In ${channelm}`)
+        .setDescription (message.content)
+        .setTimestamp ()
+        .setFooter (`${messages100.count} messages sent within the last hour.`)
+        stateswebhook.send (embed);
+    };
 })
 
 // Confirming the bots token
